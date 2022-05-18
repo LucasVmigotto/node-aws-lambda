5f0720fc11357f72aaa50941cc836745092dd1db
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-05-18 19:53:43 -0300

    fix(app): adjust lambda handler method

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
+import {
+  APIGatewayEvent,
+  APIGatewayProxyResult,
+  Context
+} from 'aws-lambda'
 import { inverse } from './utils'
 
-export const handler: APIGatewayProxyHandler = (event, _context, callback) => {
-  const name: string | null = event.body ? inverse(JSON.parse(event.body).name) : null
-  callback(
-    null,
-    {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `Hello World ${name}`.trim()
-      })
-    }
-  )
+export const handler = (
+  event: APIGatewayEvent,
+  _context: Context
+): Promise<APIGatewayProxyResult> => {
+  const { body }: { body?: string | any } = event
+  const name: string | null = body
+    ? inverse(JSON.parse(body).name)
+    : null
+  return new Promise<APIGatewayProxyResult>(resolve => resolve({
+    statusCode: 200,
+    body: JSON.stringify({ message: `Hello World ${name}`.trim() })
+  }))
 }
