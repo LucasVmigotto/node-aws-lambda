b8b05d230df5d99446dee1b3efdbf21b68225b85
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2022-05-18 20:25:42 -0300

    feat(env): add env vars to logger and node env

--- src/confg.ts
+++ src/confg.ts
@@ -1,4 +1,6 @@
 export default {
+  NODE_ENV: process.env.NODE_ENV ?? 'development',
+  LOG_LEVEL: process.env.LOG_LEVEL ?? 'debug',
   API_HOST: process.env.API_HOST ?? '0.0.0.0',
   API_PORT: process.env.API_PORT ?? 3000
 }
